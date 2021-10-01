310804ef18fbe3d832f85d7298cfa9ca2699bfa9
Djsmk123 <74850678+Djsmk123@users.noreply.github.com>
2021-10-01 19:20:05 +0530

    Delete lib directory

--- lib/editcontroller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-class EditContoller extends GetxController {
-  var isEditText=[].obs;
-  var addController=<TextEditingController>[].obs;
-}
